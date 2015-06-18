21f9cf914b93cee1d3a6bc69734e1d13bb35f4f3
YukaiZou <yukaizou2012@gmail.com>
2015-06-17 23:52:38 -0700

    plot2 files added

--- plot1.R
+++ plot1.R
@@ -2,5 +2,5 @@ library(sqldf)
 data <- read.csv.sql("household_power_consumption.txt", sql = 'select * from file where Date in ("1/2/2007", "2/2/2007")', sep = ";")
 x <- data$Global_active_power
 png(file = "plot1.png")
-hist(x, main = "Global Active Power", xlab = "global Active Power (kilowatts)", col = "red", bg = "transparent")
+hist(x, main = "Global Active Power", xlab = "global Active Power (kilowatts)", col = "red", bg = NA)
 dev.off()
