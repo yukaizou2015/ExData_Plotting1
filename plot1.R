d4cb725a479d90b15253f94f82740af458b43dca
Yukai Zou <yukaizou2012@gmail.com>
2015-08-03 13:00:16 -0700

    8.3 Plot1 updated

--- plot1.R
+++ plot1.R
@@ -1,6 +1,9 @@
-library(sqldf)
+library(xlsx, lib.loc = "//R01SFCHSM03.r01.med.va.gov/homedir$/vhasfczouy/My Documents/Coursera/library/")
+library(proto, lib.loc = "//R01SFCHSM03.r01.med.va.gov/homedir$/vhasfczouy/My Documents/Coursera/library/")
+library(sqldf, lib.loc = "//R01SFCHSM03.r01.med.va.gov/homedir$/vhasfczouy/My Documents/Coursera/library/")
+
 data <- read.csv.sql("household_power_consumption.txt", sql = 'select * from file where Date in ("1/2/2007", "2/2/2007")', sep = ";")
 x <- data$Global_active_power
-png(file = "plot1.png")
+png(file = "plot1.png", width = 480, height = 480)
 hist(x, main = "Global Active Power", xlab = "global Active Power (kilowatts)", col = "red", bg = NA)
 dev.off()
