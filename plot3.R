4fa7bbae4f65ce79bb1a88388949988b4e0d6784
Yukai Zou <yukaizou2012@gmail.com>
2015-08-03 13:14:18 -0700

    8.3 Plot3 updated

--- plot3.R
+++ plot3.R
@@ -1,16 +1,19 @@
 library(sqldf)
+setwd("C:/Users/vhasfczouy/Desktop/Coursera/exdata-data-household_power_consumption")
 data <- read.csv.sql("household_power_consumption.txt", sql = 'select * from file where Date in ("1/2/2007", "2/2/2007")', sep = ";")
+setwd("C:/Users/vhasfczouy/Desktop/Coursera/ExData_Plotting1")
+
 date_time <- with(data, paste(Date, Time))
 date_time <- strptime(date_time, "%d/%m/%Y %H:%M:%S")
-data <- data.frame(data, date_time)
-Thu <- data$date_time[1]
-Fri <- data$date_time[length(data$date_time)/2]
-Sat <- data$date_time[length(data$date_time)]
+data2 <- data.frame(data, date_time)
+Thu <- data2$date_time[1]
+Fri <- data2$date_time[length(data2$date_time)/2]
+Sat <- data2$date_time[length(data2$date_time)]
 
 png(file = "plot3.png")
-with(data, plot(date_time, Sub_metering_1, type = 'l', col = 'gray', xaxt = 'n', xlab = NA, ylab = 'Energy sub metering', bg = NA))
-lines(data$date_time, data$Sub_metering_2, col = 'red')
-lines(data$date_time, data$Sub_metering_3, col = 'blue')
+with(data2, plot(date_time, Sub_metering_1, type = 'l', xaxt = 'n', xlab = NA, ylab = 'Energy sub metering', bg = NA))
+lines(data2$date_time, data2$Sub_metering_2, col = 'red')
+lines(data2$date_time, data2$Sub_metering_3, col = 'blue')
 axis(1, at = c(Thu, Fri, Sat), labels = c("Thu", "Fri", "Sat"))
-legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex = 0.7, pt.cex = 1, lty = 1, col = c("gray", "red", "blue"))
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), cex = 0.7, pt.cex = 1, lty = 1, col = c("black", "red", "blue"))
 dev.off()
