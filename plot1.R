44af92bbdb71110ae5d35a1528967e1db4872eac
Yukai Zou <yukaizou2012@gmail.com>
2015-08-06 09:41:09 -0700

    8.6 plot1 update

--- plot1.R
+++ plot1.R
@@ -1,9 +1,12 @@
-library(xlsx, lib.loc = "//R01SFCHSM03.r01.med.va.gov/homedir$/vhasfczouy/My Documents/Coursera/library/")
 library(proto, lib.loc = "//R01SFCHSM03.r01.med.va.gov/homedir$/vhasfczouy/My Documents/Coursera/library/")
 library(sqldf, lib.loc = "//R01SFCHSM03.r01.med.va.gov/homedir$/vhasfczouy/My Documents/Coursera/library/")
 
+# Import Data
+setwd("C:/Users/vhasfczouy/Desktop/Coursera/exdata-data-household_power_consumption")
 data <- read.csv.sql("household_power_consumption.txt", sql = 'select * from file where Date in ("1/2/2007", "2/2/2007")', sep = ";")
-x <- data$Global_active_power
-png(file = "plot1.png", width = 480, height = 480)
-hist(x, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red", bg = NA)
-dev.off()
+setwd("C:/Users/vhasfczouy/Desktop/Coursera/ExData_Plotting1")
+
+x <- data$Global_active_power # Extract global active power out and assign to a variable x
+png("plot1.png", width = 480, height = 480) # Initialize graphic device for png, set output filename, width, and height
+        hist(x, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col = "red") # Plot the histogram
+dev.off() # Turn off the graphic device
